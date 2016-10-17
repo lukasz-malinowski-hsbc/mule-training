55144e9a348f4fc70268738e70bf5870f70ef466
43996131 <lukasz.g.malinowski@hsbc.com>
2016-10-17 14:24:13 +0200

    second commit

--- src/test/java/com/mulesoft/training/HelloMavenTest.java
+++ src/test/java/com/mulesoft/training/HelloMavenTest.java
@@ -7,7 +7,7 @@ import org.mule.tck.junit4.FunctionalTestCase;
 
 public class HelloMavenTest extends FunctionalTestCase {
 
-    @Test
+//    @Test
     public void mavenFlowReturnsHelloMaven() throws Exception {
         runFlowAndExpect("mavenFlow", "Hello Maven");
     }
